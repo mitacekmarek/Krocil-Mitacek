da5721d79f807b84ac2096634b467e6693266d42
bertonyyy <fanda.kro@seznam.cz>
2026-05-20 08:37:27 +0200

    pridani stranek

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/mitacek/km_projekt/Krocil-Mitacek';
+    public string $baseURL = 'http://localhost/krocilf/km_projekt';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
